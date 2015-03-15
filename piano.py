7bf7d4a09ab7082d0864233392fdd8171c6a5c1e
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2015-03-15 01:02:01 -0600

    an accurate way to set block positions

--- piano.py
+++ piano.py
@@ -3,44 +3,47 @@ from pygame.locals import *
 from classes import Background, Block
 from datetime import datetime
 
+notes  = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+scales = [1, 2, 3, 4]
+
+tile_positions = {(note, scale):(460+(scale-1)*80, 500-(80*note_index)) for note_index, note in enumerate(notes) for scale in scales}
+
 def main():
     # Initialise screen
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((800, 600)) # FULLSCREEN
     pygame.display.set_caption('Piano')
 
     # Fill background
     background = Background([0,0])
+    screen.fill((255, 255, 255))
     screen.blit(background.image, background.rect)
+
+    sound = pygame.mixer.Sound('media/sound/5.wav')
+
+    # Blocks
+
+    block = Block(tile_positions['E', 1])
+    screen.blit(block.image, block.rect)
 
     # Blit everything to the screen
     pygame.display.flip()
 
-    sound = pygame.mixer.Sound('media/sound/13.wav')
-    
-    # Create seven blocks and their y-positions
-    block = [0]*7
-    position_y = [2, 88, 174, 260, 346, 432, 518]
-    for i in range(7):
-        block[i] = Block()
-        block[i].y = position_y[i]
-    velocity = 2
- 
     # Event loop
     while 1:
         for event in pygame.event.get():
-            if event.type == KEYDOWN and event.unicode == 'h':
+            if event.type == KEYDOWN and event.key == 27:
+                return
+            elif event.type == KEYDOWN and event.unicode == 'h':
                 sound.play()
-            if event.type == QUIT:
+            elif event.type == QUIT:
                 return
 
-        move = datetime.now().microsecond%1 == 0
+        screen.fill((255, 255, 255))
         screen.blit(background.image, background.rect)
-        for i in range(7):
-            screen.blit(block[i].image, block[i].rect)
-            if  move and block[i].x>150: block[i].x-=velocity
-            else : block[i].x = 800
-            block[i].move()
+
+        screen.blit(block.image, block.rect)
+
         pygame.display.flip()
 
 
