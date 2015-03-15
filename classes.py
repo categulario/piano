7427dcd37292872e9796a0cfdab95cc57e841ce3
jesus <jesusmejded@gmail.com>
2015-03-14 20:24:47 -0600

    with motion

--- classes.py
+++ classes.py
@@ -13,3 +13,17 @@ class RedLine(pygame.sprite.Sprite):
         self.image = pygame.image.load('media/img/background.png')
         self.rect  = self.image.get_rect()
         self.rect.left, self.rect.top = location
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self):
+        self.velocity = 1
+        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        self.image = pygame.image.load('media/img/black.png')
+        self.rect  = self.image.get_rect()
+        self.x = 800
+        self.y = 0
+        self.rect.left = self.x
+        self.rect.top =  self.y
+    def move(self):
+        self.rect.left =  self.x
+        self.rect.top =  self.y
