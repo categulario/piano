d7fa714c781d77aa8384a654a6eeca2804a319c7
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2015-03-15 01:00:50 -0600

    sumplify classes

--- classes.py
+++ classes.py
@@ -2,28 +2,22 @@ import pygame
 
 class Background(pygame.sprite.Sprite):
     def __init__(self, location):
-        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
-        self.image = pygame.transform.scale(pygame.image.load('media/img/background.png'), (800, 600))
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('media/img/background.png')
         self.rect  = self.image.get_rect()
         self.rect.left, self.rect.top = location
 
 class RedLine(pygame.sprite.Sprite):
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
-        self.image = pygame.image.load('media/img/background.png')
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('media/img/line.png')
         self.rect  = self.image.get_rect()
         self.rect.left, self.rect.top = location
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, location):
         self.velocity = 1
-        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('media/img/black.png')
         self.rect  = self.image.get_rect()
-        self.x = 800
-        self.y = 0
-        self.rect.left = self.x
-        self.rect.top =  self.y
-    def move(self):
-        self.rect.left =  self.x
-        self.rect.top =  self.y
+        self.rect.left, self.rect.top =  location
