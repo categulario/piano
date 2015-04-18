dc3ab0c0fe4c5b8b237341d6518988f7562a5683
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2015-04-17 20:52:44 -0500

    base levels file

---
+++ conf/levels.py
@@ -0,0 +1,55 @@
+LEVELS = {
+    "level0": {
+        "criteria": 5,
+        "screens": [
+            "general1",
+            "general2",
+            "general3",
+            "level0_1",
+            "level0_2",
+        ]
+    },
+    "level1": {
+        "criteria": 5,
+        "screens": [
+            "general1",
+            "general2",
+            "general3",
+            "level1_1",
+            "level1_2",
+        ]
+    },
+    "level2": {
+        "criteria": 5,
+        "screens": [
+            "general1",
+            "general2",
+            "general3",
+            "level2_1",
+            "level2_2",
+        ]
+    },
+    "level3": {
+        "criteria": 5,
+        "screens": [
+            "general1",
+            "general2",
+            "general3",
+            "level3_1",
+            "level3_2",
+        ]
+    },
+}
+
+GROUPS = {
+    "groups": {
+        "group1": [
+            "level0",
+            "level1",
+        ],
+        "group2": [
+            "level2",
+            "level3",
+        ],
+    }
+}
