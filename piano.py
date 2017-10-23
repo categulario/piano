003371205b55112b60311015ddbdfda99f0818b5
Jesús Mejía <jesusmejded@gmail.com>
2017-10-23 00:22:20 -0500

    new_column to global

--- piano.py
+++ piano.py
@@ -148,6 +148,7 @@ def main(session):
                 # Handles window close button
                 return
 
+        new_column = 1
         # Move the redline
         if move:
             new_progress = redline.move()
